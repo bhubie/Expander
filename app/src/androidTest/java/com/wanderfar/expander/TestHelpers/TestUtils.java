7daf15ccae77981506c4dd1e785bdf947d6a9562
Brett Huber <bretthuber6@gmail.com>
2016-10-23 17:59:57 -0500

    Added espresso tests for Macro activity as well as helper methods

---
+++ app/src/androidTest/java/com/wanderfar/expander/TestHelpers/TestUtils.java
@@ -0,0 +1,80 @@
+/*
+ * Expander: Text Expansion Application
+ * Copyright (C) 2016 Brett Huber
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.wanderfar.expander.TestHelpers;
+
+
+import android.support.annotation.NonNull;
+import android.support.test.espresso.matcher.BoundedMatcher;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+
+
+import static android.support.test.espresso.core.deps.guava.base.Preconditions.checkNotNull;
+
+
+public class TestUtils {
+
+    public static Matcher<View> atPosition(final int position, @NonNull final Matcher<View> itemMatcher) {
+        checkNotNull(itemMatcher);
+        return new BoundedMatcher<View, RecyclerView>(RecyclerView.class) {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("has item at position " + position + ": ");
+                itemMatcher.describeTo(description);
+            }
+
+            @Override
+            protected boolean matchesSafely(final RecyclerView view) {
+                RecyclerView.ViewHolder viewHolder = view.findViewHolderForAdapterPosition(position);
+                if (viewHolder == null) {
+                    // has no item on such position
+                    return false;
+                }
+                return itemMatcher.matches(viewHolder.itemView);
+            }
+        };
+    }
+
+    public static Matcher<View> hasErrorText(final String expectedError) {
+        return new BoundedMatcher<View, View>(View.class) {
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("with error: " + expectedError);
+            }
+
+            @Override
+            protected boolean matchesSafely(View view) {
+
+                if (!(view instanceof EditText)) {
+                    return false;
+                }
+
+                EditText editText = (EditText) view;
+
+                return expectedError.equals(editText.getError());
+            }
+        };
+    }
+
+}
