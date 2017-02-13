7d79078e398b47f47ff9262fc36cd5b3091f9f1f
Brett Huber <bretthuber6@gmail.com>
2017-02-12 22:00:50 -0600

    renamed tests in MainActivityTest to better describe what they are testing

--- app/src/androidTest/java/com/wanderfar/expander/MainActivityTest.java
+++ app/src/androidTest/java/com/wanderfar/expander/MainActivityTest.java
@@ -68,9 +68,8 @@ public class MainActivityTest {
     }
 
     @Test
-    public void testNoMacroFoundMessageIsDisplayed(){
-        //Test that if no macros were loaded that no macros found message is visible
-
+    public void whenNoMacroIsSavedNoMacroFoundMessageShouldBeDisplayed(){
+=
         //Launch the main activity
         Intent intent = new Intent();
         mActivityRule.launchActivity(intent);
@@ -81,8 +80,7 @@ public class MainActivityTest {
     }
 
     @Test
-    public void testNoMacroFoundMessageIsNotDisplayed(){
-        //Test that if a macro is loaded that the no macro found message is not displayed
+    public void whenAMacroIsLoadedNoMacroFoundMessageShouldNotBeDisplayed(){
 
         //create a generic test macro and save it
         //Save the Macro
@@ -96,170 +94,4 @@ public class MainActivityTest {
         onView(withId(R.id.noMacroFound)).check(isGone());
     }
 
-
-    /*@Test
-    public void testThatIfAccessibilityPermissionForAppIsOffThatSnackbarAppears() {
-
-
-        //check and/or set the accessibility permission for the application
-        try {
-            setAccessibilitySettings("Off");
-        } catch (UiObjectNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        //onView()
-
-        try {
-            launchApp();
-        } catch (UiObjectNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        onView(allOf(withId(android.support.design.R.id.snackbar_text),
-                withText("Please enable Accessibility Settings permission for " + mActivityRule.getActivity().getResources().getString(R.string.app_name) + ". This is a required permission for the app to work.")))
-                .check(matches(isDisplayed()));
-
-
-    }
-
-
-
-    @Test
-    public void testThatIfAccessibilityPermissionForAppIsOnThatSnackbarDoesntAppear() {
-        //If the accessibility permission for the application is on, when launching the application
-        //the snack-bar that prompts the user to enable the permission should not appear
-
-        try {
-            setAccessibilitySettings("On");
-
-        } catch (UiObjectNotFoundException e) {
-            e.printStackTrace();
-        }
-
-
-       try {
-            launchApp();
-        } catch (UiObjectNotFoundException e) {
-            e.printStackTrace();
-        }
-        mDevice.waitForIdle();
-       *//* onView(allOf(withId(android.support.design.R.id.snackbar_text),
-                withText("Please enable Accessibility Settings permission for " + mActivityRule.getActivity().getResources().getString(R.string.app_name) + ". This is a required permission for the app to work.")))
-                .check(doesNotExist());*//*
-
-    }
-
-    private void launchApp() throws UiObjectNotFoundException {
-
-        mDevice.pressHome();
-
-        UiObject allAppsButton = new UiObject(LauncherHelper.ALL_APPS_BUTTON);
-        allAppsButton.click();
-
-        // Clicking the Settings
-        UiScrollable allAppsScreen = new UiScrollable(LauncherHelper.LAUNCHER_CONTAINER);
-
-
-        allAppsScreen.setAsVerticalList();
-
-        allAppsScreen.setAsVerticalList();
-        allAppsScreen.scrollTextIntoView(mApplicationName);
-
-        UiObject app = new UiObject(
-                new UiSelector().text(mApplicationName));
-        app.click();
-
-    }
-
-    public static class LauncherHelper {
-        public static final UiSelector ALL_APPS_BUTTON = new UiSelector().description("Apps");
-        public static final UiSelector LAUNCHER_CONTAINER = new UiSelector().scrollable(true);
-        public static final UiSelector LAUNCHER_ITEM =
-                new UiSelector().className(android.widget.TextView.class.getName());
-                //new UiSelector().description(android.widget.TextView.class.getName());
-                //new UiSelector().description("Settings");
-        //public static final UiSelector SETTINGS_BUTTON = new UiSelector().description("Settings");
-    }
-
-
-    private void setAccessibilitySettings(String turnSettingsOnOrOff) throws UiObjectNotFoundException {
-
-        mDevice.pressHome();
-
-        UiObject allAppsButton = new UiObject(LauncherHelper.ALL_APPS_BUTTON);
-        allAppsButton.click();
-
-        // Clicking the Settings
-        UiScrollable allAppsScreen = new UiScrollable(LauncherHelper.LAUNCHER_CONTAINER);
-
-        allAppsScreen.setAsVerticalList();
-
-        allAppsScreen.setAsVerticalList();
-        allAppsScreen.scrollTextIntoView(SETTINGS_APP_NAME);
-
-
-        UiObject settingsApp = new UiObject(
-                new UiSelector().text(SETTINGS_APP_NAME));
-        settingsApp.click();
-
-        UiScrollable settingsScreen = new UiScrollable(LauncherHelper.LAUNCHER_CONTAINER);
-
-        settingsScreen.setAsVerticalList();
-        settingsScreen.scrollTextIntoView(ACCESSIBILITY_SETTINGS_NAME);
-
-        UiObject accessibilityApp = new UiObject(
-                new UiSelector().text(ACCESSIBILITY_SETTINGS_NAME));
-
-        accessibilityApp.click();
-
-        UiScrollable accessibilityScreen = new UiScrollable(LauncherHelper.LAUNCHER_CONTAINER);
-        accessibilityScreen.setAsVerticalList();
-        accessibilityScreen.scrollTextIntoView(mApplicationName);
-
-        UiObject expanderApp = new UiObject(
-                new UiSelector().text(mApplicationName));
-
-        expanderApp.click();
-
-        UiScrollable texterScreen = new UiScrollable(LauncherHelper.LAUNCHER_CONTAINER);
-        texterScreen.setAsVerticalList();
-
-        mAccessibilityPermissionSetting = getUIObjectForAccessibilityPermissionSetting();
-
-        if (turnSettingsOnOrOff.equals("Off") && !isPermissionOff) {
-            clickPermissionToEnableOrDisable();
-        }
-        else if (turnSettingsOnOrOff.equals("On") && isPermissionOff){
-            clickPermissionToEnableOrDisable();
-        }
-
-
-    }
-
-    private void clickPermissionToEnableOrDisable() throws UiObjectNotFoundException {
-       mAccessibilityPermissionSetting.click();
-
-        UiScrollable permissionScreen = new UiScrollable(LauncherHelper.LAUNCHER_CONTAINER);
-        permissionScreen.setAsVerticalList();
-
-        UiObject permissionButton = new UiObject(
-                new UiSelector().text("OK"));
-
-        permissionButton.click();
-    }
-
-    private UiObject getUIObjectForAccessibilityPermissionSetting(){
-        UiObject permission = new UiObject(
-                new UiSelector().text("Off"));
-
-        if (permission.exists()){
-            isPermissionOff = true;
-            return permission;
-        } else {
-            UiObject permissionOn  = new UiObject(new UiSelector().text("On"));
-            isPermissionOff = false;
-            return permissionOn;
-        }
-    }*/
 }
